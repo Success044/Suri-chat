b5a60f13bf0b36604acc6451b08500c2cdcfe086
Success Maharjan <successmaharjan10@gmail.com>
2024-10-22 11:44:39 +0545

    Tailwind setup button component

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,55 @@
+import { cva, VariantProps } from "class-variance-authority";
+import { Loader2 } from "lucide-react";
+import { ButtonHTMLAttributes, FC } from "react";
+import { cn } from "@/lib/utils";
+
+const buttonVariants = cva(
+  "active:scale-95 inline-flex items-center justify-center rounded-md text-sm font-medium transition-color focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none",
+  {
+    variants: {
+      variants: {
+        default: "bg-slate-900 text-white hover:bg-slate-800",
+        ghost: "bg-transparent hover:text-slate-900 hover:bg-slate-200",
+      },
+      sizes: {
+        default: "h-10 py-2 px-4",
+        sm: "h-9 px-2",
+        lg: "h-11 px-8",
+      },
+    },
+    defaultVariants: {
+      variants: "default",
+      sizes: "default",
+    },
+  }
+);
+
+//makeing typeSafe
+export interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  isLoading?: boolean;
+}
+
+const Button: FC<ButtonProps> = ({
+  className,
+  children,
+  variants,
+  isLoading,
+  sizes,
+  ...props
+}) => {
+  return (
+    <button
+      //cn allows use to over-write the css-Style from anywhere we use Buttons
+      className={cn(buttonVariants({ variants, sizes, className }))}
+      disabled={isLoading}
+      {...props}
+    >
+      {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}
+      {children}
+    </button>
+  );
+};
+
+export default Button;
