d07ead7dfecf3a2bcba9d5853c173416b12e2f19
Success Maharjan <successmaharjan10@gmail.com>
2024-10-31 12:01:37 +0545

    accept_deny: middleware_check

--- src/components/FriendRequests.tsx
+++ src/components/FriendRequests.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { Check, UserPlus, X } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { FC, useState } from "react";
+import axios from "axios";
 
 interface FriendRequestsProps {
   incomingFriendRequests: IncomingFriendRequest[];
@@ -11,9 +13,25 @@ const FriendRequests: FC<FriendRequestsProps> = ({
   incomingFriendRequests,
   sessionId,
 }) => {
+  const router = useRouter();
   const [FriendRequests, setFriendRequets] = useState<IncomingFriendRequest[]>(
     incomingFriendRequests
   );
+
+  const acceptFriend = async (senderId: string) => {
+    await axios.post("/api/friends/accept", { id: senderId });
+    setFriendRequets((prev) =>
+      prev.filter((request) => request.senderId !== senderId)
+    );
+    router.refresh();
+  };
+  const denyFriend = async (senderId: string) => {
+    await axios.post("/api/friends/deny", { id: senderId });
+    setFriendRequets((prev) =>
+      prev.filter((request) => request.senderId !== senderId)
+    );
+    router.refresh();
+  };
   return (
     <>
       {FriendRequests.length === 0 ? (
@@ -26,12 +44,14 @@ const FriendRequests: FC<FriendRequestsProps> = ({
             <button
               aria-label="accept friend"
               className="w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md"
+              onClick={() => acceptFriend(request.senderId)}
             >
               <Check className="font-semibold text-white w-3/4 h-3/4" />
             </button>
             <button
               aria-label="deny friend"
               className="w-8 h-8 bg-red-700 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md"
+              onClick={() => denyFriend(request.senderId)}
             >
               <X className="font-semibold text-white w-3/4 h-3/4" />
             </button>
